5b819c3bbc2c5b9098fdcd799de085b793bc0b97
Ooglely <aahgr@mst.edu>
2023-03-08 10:52:46 -0600

    Add user agent check, fix metadata tags

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -9,55 +9,71 @@ const baseURL = process.env.RAILWAY_STATIC_URL || "localhost:3000";
 async function writeLogData(id) {
   const getStream = bent('https://logs.tf/')
 
-    let stream = await getStream(id)
-    const html = await stream.text()
+  let stream = await getStream(id)
+  const html = await stream.text()
 
-    var $ = load(html);
-    var title = $("title").text();
-    var map = $("#log-map").text();
-    var length = $("#log-length").text();
-    
-    var log_title = title.split(" – logs.tf")[0];
-    var log_description = map + " - " + length;
-    var log_link = "https://logs.tf/" + id;
-    var log_image = baseURL + '/img/' + id + '.png';
-    
-    // Make sure img dir is created
-    if (!fs.existsSync("img/")) {
-      await fs.promises.mkdir("img/")
-    }
-    
-    if (!fs.existsSync("img/" + id + ".png")) {
-      await captureWebsite.file(log_link, 'img/' + id + '.png', {
-        element: "#log-section-players",
-        removeElements: ["#log-section-teams", "#log-section-rounds", "#log-section-healspread", "#log-section-cvc", "#log-section-footer", "body > div.container.main > footer"],
-        inset: {
-          top: -80,
-          left: 20,
-          right: 20,
-          bottom: 0
-        },
-        launchOptions: {
-          args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        }
-      });
-    }
+  var $ = load(html);
+  var title = $("title").text();
+  var map = $("#log-map").text();
+  var length = $("#log-length").text();
+  
+  var log_title = title.split(" – logs.tf")[0];
+  var log_description = map + " - " + length;
+  var log_link = "https://logs.tf/" + id;
+  var log_image = "https://" + baseURL + '/img/' + id + '.png';
+  
+  // Make sure img dir is created
+  if (!fs.existsSync("img/")) {
+    await fs.promises.mkdir("img/")
+  }
+  
+  if (!fs.existsSync("img/" + id + ".png")) {
+    await captureWebsite.file(log_link, 'img/' + id + '.png', {
+      element: "#log-section-players",
+      removeElements: ["#log-section-teams", "#log-section-rounds", "#log-section-healspread", "#log-section-cvc", "#log-section-footer", "body > div.container.main > footer"],
+      inset: {
+        top: -80,
+        left: 20,
+        right: 20,
+        bottom: 0
+      },
+      launchOptions: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      }
+    });
+  }
 
-    var log_data = {
-      title: log_title,
-      description: log_description,
-      link: log_link,
-      image: log_image
-    }
+  var log_data = {
+    title: log_title,
+    description: log_description,
+    link: log_link,
+    image: log_image
+  }
 
-    var log_json = JSON.stringify(log_data);
-    console.log(log_json);
-    await fs.promises.writeFile('logs/' + id + '.json', log_json);
+  var log_json = JSON.stringify(log_data);
+  console.log(log_json);
+  await fs.promises.writeFile('logs/' + id + '.json', log_json);
 }
 
 export async function getServerSideProps(context) {
+
   var id = context.query.id;
   id = id.split('#')[0];
+
+  // If user agent is detected to be a crawler, let the req go through
+  // else, redirect to logs page
+  const userAgent = context.req.headers['user-agent'] || '';
+  // This bot regex is taken from the FixTweet repo, thanks to them
+  const isBot = userAgent.match("/bot|facebook|embed|got|firefox\/92|firefox\/38|curl|wget|go-http|yahoo|generator|whatsapp|preview|link|proxy|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node/gi") !== null;
+
+  if (!isBot) {
+    return {
+      redirect: {
+        destination: 'https://logs.tf/' + id,
+        permanent: true,
+      },
+    }
+  }
 
   // Make sure logs dir is created
   if (!fs.existsSync("logs/")) {
@@ -90,13 +106,10 @@ export default function Home({ title, description, link, image }) {
       <meta content={link} property="og:url" />
       <meta content={image} property="og:image" />
       <meta property="og:type" content="website" />
-      <meta name="theme-color" content="#FF0000" />
+      <meta name="theme-color" content="#444444" />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={image} />
       </Head>
-      <div><p>Log Title: {title}</p></div>
-      <div><p>Description: {description}</p></div>
-      <div><p>Log Link: {link}</p></div>
-      <div><p>Log Image: {image}</p></div>
     </>
   )
 }
